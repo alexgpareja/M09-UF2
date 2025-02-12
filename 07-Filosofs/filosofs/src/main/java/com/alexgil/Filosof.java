b7c3c94e55177f62f4f39fbecb27f699076dcd66
Alex Gil <alexgpareja@icloud.com>
2025-02-12 20:07:55 +0100

    esquelet filosof

--- 07-Filosofs/filosofs/src/main/java/com/alexgil/Filosof.java
+++ 07-Filosofs/filosofs/src/main/java/com/alexgil/Filosof.java
@@ -1,5 +1,27 @@
 package com.alexgil;
 
-public class Filosof {
+public class Filosof extends Thread {
     
+    // Propietats
+    private String nom;
+    private final Forquilla forquillaEsquerra;
+    private final Forquilla forquillaDreta;
+    private int comptadorGana;
+
+    public Filosof(String nom) {
+        this.nom = nom;
+    }
+
+    public void menjar() {
+
+    }
+
+    public void pensar() {
+
+    }
+
+    public void run() {
+
+    }
+
 }
