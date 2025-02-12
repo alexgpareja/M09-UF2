231a317e13dc8a8ae6ac8e219091429a0a5f2082
Alex Gil <alexgpareja@icloud.com>
2025-02-12 19:44:19 +0100

    classe forquilla creada

--- 07-Filosofs/filosofs/src/main/java/com/alexgil/Forquilla.java
+++ 07-Filosofs/filosofs/src/main/java/com/alexgil/Forquilla.java
@@ -1,5 +1,27 @@
 package com.alexgil;
 
 public class Forquilla {
-    
+
+    // Propietats
+    private final int num;
+    private boolean enUs;
+
+    // Constructor per defecte
+    public Forquilla(int num) {
+        this.num = num;
+        enUs = false;
+    }
+
+    // Getters i setters
+    public int getNum() {
+        return num;
+    }
+
+    public boolean isEnUs() {
+        return enUs;
+    }
+
+    public void setEnUs(boolean enUs) {
+        this.enUs = enUs;
+    }
 }
