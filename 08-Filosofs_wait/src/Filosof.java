6e3a926e2df770311263a926b4e06dd9fcf91868
Alex Gil <alexgpareja@icloud.com>
2025-02-19 19:48:37 +0100

    activitat 8 - esquelet filosof completat

--- 08-Filosofs_wait/src/Filosof.java
+++ 08-Filosofs_wait/src/Filosof.java
@@ -1,5 +1,47 @@
-public class Filosof {
+import java.util.Random;
 
-    
-    
+public class Filosof extends Thread {
+
+    // Propietats
+    private String nom;
+    private Forquilla forquillaEsquerra;
+    private Forquilla forquillaDreta;
+    private Random random;
+
+    // Constructor
+    public Filosof(String nom, Forquilla forquillaEsquerra, Forquilla forquillaDreta) {
+        this.nom = nom;
+        this.forquillaEsquerra = forquillaEsquerra;
+        this.forquillaDreta = forquillaDreta;
+        this.random = new Random();
+    }
+
+    // Getters
+    public String getNom() {
+        return nom;
+    }
+
+    public Forquilla getForquillaEsquerra() {
+        return forquillaEsquerra;
+    }
+
+    public Forquilla getForquillaDreta() {
+        return forquillaDreta;
+    }
+
+    // Mètode menjar
+    public void menjar() {
+    }
+
+    // Mètode pensar
+    public void pensar() {
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            pensar();
+            menjar();
+        }
+    }
 }
