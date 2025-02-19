34af64903e6832fd5a4681b3938601c6ee460829
Alex Gil <alexgpareja@icloud.com>
2025-02-19 19:46:14 +0100

    Activitat 8 - esquelet taula completat

--- 08-Filosofs_wait/src/Taula.java
+++ 08-Filosofs_wait/src/Taula.java
@@ -1,3 +1,36 @@
 public class Taula {
     
+    // Propietats
+    private Filosof[] comensals;
+    private Forquilla[] forquilles;
+
+    // Constructor
+    public Taula(int numFilosofs) {
+        // Inicialitzar els arrays
+        comensals = new Filosof[numFilosofs];
+        forquilles = new Forquilla[numFilosofs];
+
+        // Crear les forquilles
+        for (int i = 0; i < numFilosofs; i++) {
+            forquilles[i] = new Forquilla(i);
+        }
+
+        // Crear els filosofs, i assignar les forquilles a cada un
+        for (int i = 0; i < numFilosofs; i++) {
+            comensals[i] = new Filosof("Filosof-" + i, forquilles[i], forquilles[(i + 1) % numFilosofs]);
+        }
+    }
+
+    // Mètode per mostrar la taula
+    public void showTaula() {}
+
+    // Mètode per iniciar els fils dels filòsofs
+    public void cridarATaula() {}
+
+    // Mètode principal
+    public static void main(String[] args) {
+        Taula taula = new Taula(5); // Crear una taula amb 5 filòsofs
+        taula.showTaula();
+        taula.cridarATaula();
+    }
 }
